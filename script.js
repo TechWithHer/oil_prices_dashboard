9d8e3bfaff6868b6c1007a719203dabd48898fa7
Ayushi <ayushi2530@gmail.com>
2026-03-17 17:10:19 +0800

    Fix CI/CD and frontend data mapping

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const countries = ["US", "China", "India", "Russia", "Singapore", "UAE", "Iran"];
 
-fetch("data/oil_prices.json")
+  fetch("data/oil_prices.json")
   .then(res => res.json())
   .then(data => {
     const table = document.getElementById("oilTable");
 
-    countries.forEach(country => {
+    data.forEach(item => {
       const row = document.createElement("tr");
+
       row.innerHTML = `
-        <td>${country}</td>
-        <td>$${data[country] || "N/A"}</td>
+        <td>${item.country}</td>
+        <td>$${item.price}</td>
       `;
+
       table.appendChild(row);
     });
   })
-  .catch(error => {
-    console.error("Error fetching oil prices:", error);
-  });
+  .catch(err => console.error(err));
